94e7946da9826c30031bc78ed6d6da37320b9b99
Andrius Chamentauskas <sinsiliux@gmail.com>
2010-07-11 23:25:04 +0300

    Fixed some errors with deprecated enable_blueprints

--- lib/blueprints/extensions/deprecated.rb
+++ lib/blueprints/extensions/deprecated.rb
@@ -1,6 +1,6 @@
 module EnableBlueprints
   def enable_blueprints(options = {})
-    puts "DEPRECATION WARNING: enable_blueprints is deprecated. Use Blueprints.enable"
+    STDERR.puts "DEPRECATION WARNING: enable_blueprints is deprecated. Use Blueprints.enable"
     Blueprints.enable do |config|
       options.each {|option, value| config.send("#{option}=", value) }
     end
@@ -8,4 +8,10 @@ module EnableBlueprints
 end
 
 ActiveSupport::TestCase.send(:include, EnableBlueprints) if defined? ActiveSupport::TestCase
-Spec::Runner::Configuration.send(:include, EnableBlueprints) if defined? Spec or $0 =~ /script.spec$/
+module Spec
+  module Runner
+    class Configuration
+      include EnableBlueprints
+    end
+  end
+end if defined? Spec or $0 =~ /script.spec$/
