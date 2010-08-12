ecabec1c0a46621f80b7a152e8dec76b75865a35
Andrius Chamentauskas <sinsiliux@gmail.com>
2010-08-13 01:40:39 +0300

    Fixed error messages to include blueprint name

--- lib/blueprints/errors.rb
+++ lib/blueprints/errors.rb
@@ -5,19 +5,19 @@ module Blueprints
     end
 
     def to_s
-      "Blueprint '#{@name}': #{message}"
+      "Blueprint '#{@name}' #{message_append}"
     end
   end
 
   class DemolishError < Error
-    def message
+    def message_append
       'must be built before demolishing'
     end
   end
 
   # Is raised when blueprint or namespace is not found.
   class BlueprintNotFoundError < Error
-    def message
+    def message_append
       'not found'
     end
   end
