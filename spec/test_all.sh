2ceb5a92fd57dbf56cf86c8d6ff6925672f1f5e9
Andrius Chamentauskas <sinsiliux@gmail.com>
2010-09-16 15:43:52 +0300

    Added colorful output and minimized cucumber output in test_all

--- spec/test_all.sh
+++ spec/test_all.sh
@@ -9,36 +9,36 @@ function e {
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 
 e "Normal spec"
-rspec spec/active_record/blueprints_spec.rb
-rspec spec/unit/*_spec.rb
+rspec -c spec/active_record/blueprints_spec.rb
+rspec -c spec/unit/*_spec.rb
 
 e "Without transactions"
-NO_TRANSACTIONS=true rspec spec/active_record/blueprints_spec.rb
+NO_TRANSACTIONS=true rspec -c spec/active_record/blueprints_spec.rb
 
 e "With no db"
-rspec spec/no_db/blueprints_spec.rb
+rspec -c spec/no_db/blueprints_spec.rb
 
 e "With Test::Unit"
 rake rspec_to_test
 ruby test/blueprints_test.rb
 
 e "With Cucumber"
-cucumber features/blueprints.feature
+cucumber features/blueprints.feature -f progress
 
 e "With Rails 2 and RSpec 1.3.0"
-RAILS=2.3.0 spec "_1.3.0_" spec/active_record/blueprints_spec.rb
+RAILS=2.3.0 spec "_1.3.0_" -c spec/active_record/blueprints_spec.rb
 
 e "With ruby 1.9.2"
 rvm 1.9.2
-rspec spec/active_record/blueprints_spec.rb
-rspec spec/unit/*_spec.rb
+rspec -c spec/active_record/blueprints_spec.rb
+rspec -c spec/unit/*_spec.rb
 
 e "With ruby 1.9.1"
 rvm 1.9.1
-RAILS=2.3.0 spec spec/active_record/blueprints_spec.rb
+RAILS=2.3.0 spec -c spec/active_record/blueprints_spec.rb
 
 e "With ruby 1.8.6"
 rvm 1.8.6
-RAILS=2.3.0 spec spec/active_record/blueprints_spec.rb
+RAILS=2.3.0 spec -c spec/active_record/blueprints_spec.rb
 
 rvm system
